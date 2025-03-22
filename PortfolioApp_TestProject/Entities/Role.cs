a44264cc48cd934ca8acb387a46fe4dfed2bf87d
MahammadA <mahammada@risk.az>
2025-03-22 20:29:35 +0400

    update files with deletions

--- PortfolioApp_TestProject/Entities/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PortfolioApp.Entities;
-
-public class Role
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
-}
